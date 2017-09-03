b1c2662c22c3a4aa75e0b8eb9ca780d0b8cbdcd5
xiaoshidefeng <1330661071@qq.com>
2017-09-03 22:21:20 +0800

    商品列表联动滚动完成

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="goods">
-    <div class="menu-wrapper" v-el:menu-wrapper>
+    <div class="menu-wrapper" ref="menuWrapper">
       <ul>
-        <li v-for="item in goods" class="menu-item">
+        <li v-for="(item, index) in goods" class="menu-item" :class="{'current':currentIndex===index}" @click="selectMenu(index, $event)">
           <span class="text border-1px">
             <span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>
             {{item.name}}
@@ -10,9 +10,9 @@
         </li>
       </ul>
     </div>
-    <div class="foods-wrapper" v-el:foods-wrapper>
+    <div class="foods-wrapper" ref="foodsWrapper">
       <ul>
-        <li v-for="item in goods" class="food-list">
+        <li v-for="item in goods" class="food-list food-list-hook">
           <h1 class="title">{{item.name}}</h1>
           <ul>
             <li v-for="food in item.foods" class="food-item border-1px">
@@ -23,8 +23,7 @@
                 <h2 class="name">{{food.name}}</h2>
                 <p class="desc">{{food.description}}</p>
                 <div class="extra">
-                  <span class="count">月售{{food.sellCount}}份</span>
-                  <span>好评率{{food.rating}}</span>
+                  <span class="count">月售{{food.sellCount}}份</span><span>好评率{{food.rating}}</span>
                 </div>
                 <div class="price">
                   <span class="now">￥{{food.price}}</span>
@@ -48,7 +47,20 @@ export default {
     return {
       seller: {},
       goods: {},
-      ratings: {}
+      ratings: {},
+      listHeight: [],
+      scrollY: 0
+    }
+  },
+  computed: {
+    currentIndex() {
+      for(let i = 0; i < this.listHeight.length; i++) {
+        let height1 = this.listHeight[i];
+        let height2 = this.listHeight[i + 1];
+        if(!height2 || (this.scrollY >= height1 && this.scrollY < height2)) {
+          return i;
+        }
+      }
     }
   },
   created() {
@@ -63,14 +75,45 @@ export default {
         this.seller = response.data.seller;
         this.ratings = response.data.ratings;
         this.goods = response.data.goods;
-        this._initScroll();
+        this.$nextTick(() => {
+          this._initScroll();
+          this._calculateHeight();
+        });
+
       })
 
     },
     _initScroll() {
-      this.meunSCroll = new BScroll(this.$els.menuWrapper, {});
+      this.meunSCroll = new BScroll(this.$refs.menuWrapper, {
+        click: true
+      });
 
-      this.foodsSCroll = new BScroll(this.$els.foodsWrapper, {});
+      this.foodsSCroll = new BScroll(this.$refs.foodsWrapper, {
+        probeType: 3
+      });
+
+      this.foodsSCroll.on('scroll', (pos) => {
+        this.scrollY = Math.abs(Math.round(pos.y));
+      });
+
+    },
+    _calculateHeight() {
+      let foodList = this.$refs.foodsWrapper.getElementsByClassName('food-list-hook');
+      let height = 0;
+      this.listHeight.push(height);
+      for(let i = 0; i < foodList.length; i++) {
+        let item = foodList[i];
+        height += item.clientHeight;
+        this.listHeight.push(height);
+      }
+    },
+    selectMenu(index, event) {
+      if(!event._constructed) {
+        return;
+      }
+      let foodList = this.$refs.foodsWrapper.getElementsByClassName('food-list-hook');
+      let el = foodList[index];
+      this.foodsSCroll.scrollToElement(el, 1000);
     }
   }
 
@@ -97,6 +140,14 @@ export default {
         width: 56px
         padding: 0 12px
         line-height: 14px
+        &.current
+          position: relative
+          z-index: 10
+          margin-top: -1px
+          background: #fff
+          font-weight: 700
+          .text
+            border-none()
         .icon
           display inline-block
           vertical-align: top
@@ -155,9 +206,10 @@ export default {
             font-size: 10px
             color: rgb(147, 153, 159)
           .desc
-             margin-bottom: 8px
+            line-height: 12px
+            margin-bottom: 8px
           .extra
-            &.count
+            .count
               margin-right: 12px
           .price
             font-weight: 700
